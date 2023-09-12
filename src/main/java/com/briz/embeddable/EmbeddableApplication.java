646ba5a49963f136154e23ac80c201348645668d
kanchan raju <kanchanraju2015@gmail.com>
2023-09-12 13:14:43 +0530

    embeddable commit

---
+++ src/main/java/com/briz/embeddable/EmbeddableApplication.java
@@ -0,0 +1,13 @@
+package com.briz.embeddable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddableApplication.class, args);
+	}
+
+}
