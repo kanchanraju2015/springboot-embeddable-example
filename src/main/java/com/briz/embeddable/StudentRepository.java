620f949c0466c8b398c4402b43a05fefedc3318f
Kanchan Raju <kanchanraju2015@gmail.com>
2023-09-13 11:25:10 +0530

    Update StudentRepository.java

--- src/main/java/com/briz/embeddable/StudentRepository.java
+++ src/main/java/com/briz/embeddable/StudentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student,Integer>
 {
 Student findByName(String name);
+Student findByAge(int age);
 }
